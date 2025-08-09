4616626d5d163b81eb5543c73bd0ac20144ce33b
Amruthavalli-M <22233@apps.eggs.school.nz>
2025-08-09 17:55:12 +1200

    Create notification_bar.dart

---
+++ lib/widgets/notification_bar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NotificationBar extends StatelessWidget {
+  const NotificationBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      elevation: 0,
+      child: Container(
+        color: Colors.amber, // Same as your amber container
+        width: double.infinity,
+        height: double.infinity,
+      ),
+    );
+  }
+}
