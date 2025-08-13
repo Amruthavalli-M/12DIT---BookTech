ba0893dc407556b85971e274df96eb187f428532
Amruthavalli-M <22233@apps.eggs.school.nz>
2025-08-13 22:30:46 +1200

    Update theme.dart
    FINAL VERSION

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyAppColor {
-  static const backgroundColor = Color(0xfff5f6fe);
-  static const secondary = Color(0xffa6a6a6);
-  static const iconGray = Color(0xff767676);
-  static const primary = Color(0xff262626);
-  static const secondaryBg = Color(0xffececf6);
-  static const barBg = Color(0xffe3e3ee);
+  static const backgroundColor = Colors.white;            // white background
+  static const secondary = Color(0xFF386FA4);              // dark blue for fonts/text
+  static const iconGray = Color(0xFF59A5D8);                // light blue for icons (subtle)
+  static const primary = Color(0xFF133C55);                 // medium blue for sidebar bg
+  static const secondaryBg = Color.fromARGB(255, 255, 255, 255); // very light blue for secondary background
+  static const barBg = Color.fromARGB(255, 165, 227, 255); // pale blue for bars or highlights
+}
 
+class MyAppTheme {
+  static ThemeData lightTheme = ThemeData(
+    scaffoldBackgroundColor: MyAppColor.backgroundColor,
+    primaryColor: MyAppColor.primary,
+    textTheme: TextTheme(
+      headlineLarge: GoogleFonts.audiowide(
+        fontSize: 28,
+        fontWeight: FontWeight.bold,
+        color: MyAppColor.primary,
+      ),
+      titleMedium: TextStyle(
+        fontSize: 18,
+        color: MyAppColor.secondary,
+      ),
+
+      titleSmall: TextStyle(
+        fontSize: 16,
+        height: 1.4,
+        color: MyAppColor.secondary,
+      )
+    ),
+  );
 }
