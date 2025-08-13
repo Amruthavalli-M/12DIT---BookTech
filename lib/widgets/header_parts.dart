f7d8381512058fb64cc712bb7bae410fdb76459d
Amruthavalli-M <22233@apps.eggs.school.nz>
2025-08-13 22:31:54 +1200

    Update header_parts.dart
    FINAL VERSION

--- lib/widgets/header_parts.dart
+++ lib/widgets/header_parts.dart
@@ -1,59 +1,35 @@
-import 'package:booktech_flutter/utils/responsive.dart';
-import 'package:booktech_flutter/utils/theme.dart';
 import 'package:flutter/material.dart';
 
 class HeaderParts extends StatelessWidget {
-  const HeaderParts({super.key});
+  // Title text passed in to show role dashboard
+  final String title;
+
+  // Title argument
+  const HeaderParts({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisSize: MainAxisSize.min,
+      mainAxisSize: MainAxisSize.min, // Row width wraps content 
       children: [
-        const SizedBox(
+        SizedBox(
+          // Container for the column of texts
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start, // Align children at top vertically
+            crossAxisAlignment: CrossAxisAlignment.start, // Align children to the left horizontally
             children: [
+              // Large title text
               Text(
-                "Dashboard", 
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w900, 
-                  height: 1.3, 
-                  color: MyAppColor.primary  
-                ),
+                title, 
+                style: Theme.of(context).textTheme.headlineLarge,
               ),
+
+              // Subtitle text below title, static string 
               Text(
                 "Technical Committee", 
-                style: TextStyle(
-                  fontSize: 16,
-                  height: 1.3, 
-                  color: MyAppColor.secondary,  
-                ),
+                style: Theme.of(context).textTheme.titleMedium,
               )
             ],
-          ),
-        ),
-       const Spacer(flex: 1), 
-        Expanded(
-          flex: Responsive.isDesktop(context) ? 1 : 3,
-          child: TextField(
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: Colors.white,
-              contentPadding: const EdgeInsets.only(left: 40, right: 5),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-                borderSide: const BorderSide(color: Colors.white),
-              ), 
-              prefixIcon: Icon(Icons.search, color: Colors.black),
-              hintText: "Search", 
-              hintStyle: TextStyle(
-                color: MyAppColor.secondary, 
-                fontSize: 14,
-              ),
-            ),
           ),
         ),
       ],
