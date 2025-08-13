e4b2c6df1662bd169c7564edf283a2b54920d994
Amruthavalli-M <22233@apps.eggs.school.nz>
2025-08-13 22:35:39 +1200

    Update main.dart
    FINAL VERSION

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,42 @@
+import 'package:booktech_flutter/utils/theme.dart';
 import 'package:flutter/material.dart';
+// Import different screen files for navigation
 import 'screens/login_screen.dart';
 import 'screens/student_dashboard.dart';
 import 'screens/teacher_dashboard.dart';
 import 'screens/leader_dashboard.dart';
 
+// Entry point of the Flutter application
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp()); // Runs the MyApp widget
 }
 
+// Root widget of the app
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // Removes the debug banner from the top right corner
       debugShowCheckedModeBanner: false,
+
+      // Title of the app 
       title: 'Technical Committee App',
-      theme: ThemeData (primarySwatch: Colors.blue),
+
+      // Sets the default theme (blue color scheme)
+      theme: MyAppTheme.lightTheme,
+
+      // First screen to be displayed when the app starts
       initialRoute: '/',
+
+      // Named routes for navigation between pages
       routes: {
-        '/': (context) => const LoginScreen(),
-        '/student_dashboard': (context) => StudentDashboard(),
-        '/teacher_dashboard': (context) => const TeacherDashboard(),
-        '/leader_dashboard': (context) => const LeaderDashboard(),
+        '/': (context) => const LoginScreen(), // Login page
+        '/student_dashboard': (context) => StudentDashboard(), // Student dashboard
+        '/teacher_dashboard': (context) => TeacherDashboard(), // Teacher dashboard
+        '/leader_dashboard': (context) => LeaderDashboard(),   // Leader dashboard
       },
     );
   }
 }
-
-
